aa329287f62c2667b088e3f4a3f89ca3c3a32ccf
sayed zakaria <sayed.zakaria@databand.sa>
2025-12-31 20:11:49 +0200

    fluent validation for update and delete product

--- Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,8 +1,18 @@
 ﻿
+using Catalog.API.Products.UpdateProduct;
+
 namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id):ICommand<DeletProductResult>;
     public record DeletProductResult(bool IsSucsess);
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+          
+        }
+    }
     internal class DeleteProductCommandHandler(IDocumentSession session,ILogger<DeleteProductCommandHandler>logger)
         : ICommandHandler<DeleteProductCommand, DeletProductResult>
     {
--- Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -5,6 +5,18 @@ namespace Catalog.API.Products.UpdateProduct
 {
     public record UpdateProductRequest(Guid Id, string Name, List<String> Category, string Description, string ImageFile, decimal Price);
     public record UpdateProductResponse(bool IsSucsess);
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductRequest>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
+                .Length(2,150).WithMessage("Name Must Be Between 2 and 150 Character ");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image file is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be grater than 0");
+        }
+    }
     public class UpdateProductEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
