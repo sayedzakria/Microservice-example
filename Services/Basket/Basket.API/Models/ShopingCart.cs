ebc9464aa5113b09a554192f69ff0646fac4cdbb
sayed zakaria <sayed.zakaria@databand.sa>
2026-01-05 20:00:57 +0200

    add models

---
+++ Services/Basket/Basket.API/Models/ShopingCart.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Models
+{
+    public class ShopingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShopingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice=>Items.Sum(item=>item.Price * item.Quantity);
+        public ShopingCart(string userName)
+        {
+            UserName = userName;
+        }
+        // Empty constructor for mapping purposes
+        public ShopingCart()
+        {
+        }
+    }
+}
