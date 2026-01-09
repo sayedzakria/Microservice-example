8981cbf58c409108af8bed00281ae7fb573e30cb
sayed zakaria <sayed.zakaria@databand.sa>
2026-01-09 19:29:03 +0200

    Register Carter, MediatR, and pipeline behaviors
    Added Carter and MediatR service registration to DI container. Configured MediatR to scan the current assembly and include ValidationBehaviors and LoggingBehavior. Updated HTTP pipeline to map Carter endpoints. Added global using for BuildingBlocks.Behaviors.

--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,16 @@
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to container 
+builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviors<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 var app = builder.Build();
 
 //configure the HTTP requests pipeline
+app.MapCarter();
 app.Run();
