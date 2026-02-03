5c3ebe64b47395c63ff50c05d13ded9183c30d2c
sayed zakaria <sayed.zakaria@databand.sa>
2026-02-03 21:49:23 +0200

    Add Domain layer abstractions

---
+++ Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+        public IDomainEvent[] clearDomainEvent()
+        {
+            IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return dequeuedEvents;
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Entity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
