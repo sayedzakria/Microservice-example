2628ec63425554c31c9124c04fb9f013e1b8fdf9
sayed zakaria <sayed.zakaria@databand.sa>
2026-02-13 21:39:48 +0200

    Adding Dtos

---
+++ Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record OrderDto
+(
+    Guid Id,
+    Guid CusromerId,
+    string OrderName,
+    AddressDto ShippingAddress,
+      AddressDto BillingAddress,
+        PaymentDto Payment,
+        OrderStatus Status,
+        List<OrderItemDto> OrderItems
+    );
---
+++ Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Ordering.Application.Dtos;
+
+public   record PaymentDto
+(   
+   string CardName,
+    string CardNumber,
+    string Expiration,
+    string Cvv,
+    int PaymentMethod
+);
