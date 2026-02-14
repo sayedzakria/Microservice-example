bcc7acfe5a93e19c3f76547d1c68c63bd9d54947
sayed zakaria <sayed.zakaria@databand.sa>
2026-02-14 15:04:27 +0200

    delete order command

---
+++ Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public record DeleteOrderCommand(Guid OrderId)
+    : ICommand<DeleteOrderResult>;
+
+public record DeleteOrderResult(bool IsSuccess);
+public class DeleteOrderValidator : AbstractValidator<DeleteOrderCommand>
+{
+    public DeleteOrderValidator()
+    {
+        RuleFor(x => x.OrderId)
+            .NotEmpty().WithMessage("OrderId is required.");
+    }
+}   
---
+++ Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder;
+
+public class DeleteOrderHandler(IApplicationDbContext dbContext)
+    : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+{
+    public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+    {
+        //Delete order entity from database using command.OrderId
+        var orderId = OrderId.Of(command.OrderId);
+        var order = await dbContext.Orders.FindAsync(orderId, cancellationToken);
+        if (order is null)
+        {
+            throw new OrderNotFoundException(command.OrderId);
+        }
+        //save changes to database
+                dbContext.Orders.Remove(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        //return result indicating success or failure
+        return new DeleteOrderResult (true);
+    }
+}
